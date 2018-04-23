1ccecfc0b8e88d24279e868214c0bf3eb10b8088
DESKTOP-4H2CDN2\Admin <trannha132@gmail.com>
2018-04-23 21:48:08 +0700

    Website: dẫn link lòng vòng

--- assets/js/trangchu.js
+++ assets/js/trangchu.js
@@ -7,4 +7,38 @@
       this.setAttribute("href", "./cart.html");
     }
   });
+  $("#dienthoai").click(function(event) {
+		window.location.href='./productView.html';
+	});
+   $("#maytinhbang").click(function(event) {
+		window.location.href='./productView.html';
+	});
+    $("#samsung").click(function(event) {
+		window.location.href='./productView.html';
+	});
+	 $("#htc").click(function(event) {
+		window.location.href='./productView.html';
+	});
+	  $("#apple").click(function(event) {
+		window.location.href='./productView.html';
+	});
+	  $("#oppo").click(function(event) {
+		window.location.href='./productView.html';
+	});
+	   $("#xiaomi").click(function(event) {
+		window.location.href='./productView.html';
+	});
+	    $("#sony").click(function(event) {
+		window.location.href='./productView.html';
+	});
+	     $("#buttonDangNhap").click(function(event) {
+		window.location.href='./login.html';
+	});
+	 $("#btnDangKy").click(function(event) {
+		window.location.href='./signup.html';
+	});
+
+	 $(".btnThanhToan").click(function(event) {
+		window.location.href='./confirmPurchase.html';
+	});
 });
