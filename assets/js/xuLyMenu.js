40430b6edbdc9f74d050e3711fafaa6aecfc4d69
DESKTOP-AH2IB63\Windows 10 <leehuunghi@gmail.com>
2018-04-27 18:56:24 +0700

    xu ly menu khac nhau giua dang nhap va chua dang nhap cho toan bo trang

--- assets/js/xuLyMenu.js
+++ assets/js/xuLyMenu.js
@@ -1,10 +1,14 @@
+var menu = document.getElementById("menuDanhMuc");
+var img = document.getElementById("menuIcon");
+var menu2 = document.getElementById("menuAccount");
+
+
 var menuToggle = function () {
-    var menu = document.getElementById("menuDanhMuc");
-    var img = document.getElementById("menuIcon");
     if (menu.style.display === "none") {
         menu.style.display = "block";
         img.src = "./assets/icons/close.png";
         event.stopPropagation();
+        menu2.style.display = "none";
     } else {
         menu.style.display = "none";
         img.src = "./assets/icons/menu.png";
@@ -12,11 +16,20 @@ var menuToggle = function () {
 }
 
 var menuTKToggle = function () {
-    var menu2 = document.getElementById("menuAccount");
     if (menu2.style.display === "none") {
         menu2.style.display = "block";
         event.stopPropagation();
+        menu.style.display = "none";
+        img.src = "./assets/icons/menu.png";
     } else {
         menu2.style.display = "none";
     }
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    document.getElementById("menuDanhMuc").style.display = "none";
+    document.getElementById("menuIcon").img.src = "./assets/icons/close.png";
+    document.getElementById("menuAccount").style.display = "none";
 }
