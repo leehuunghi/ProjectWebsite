fc2dd40b258e1ec8e0b69c6763fcc20f79226846
donguyen131197 <do.nguyen131197@gmail.com>
2018-05-17 16:43:50 +0700

    Push

--- ProjectWebVersion2/controllers/productViewController.js
+++ ProjectWebVersion2/controllers/productViewController.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('product-view/index');
+    res.render('productView/index');
 });
 
 module.exports = router;
--- ProjectWebVersion2/controllers/receiverUpdateController.js
+++ ProjectWebVersion2/controllers/receiverUpdateController.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('receiver-update/index');
+    res.render('receiverUpdate/index');
 });
 
 module.exports = router;
