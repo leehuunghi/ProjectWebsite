d02b4cb81adb3a17747630d252dc5048fe55d73c
DESKTOP-AH2IB63\Windows 10 <leehuunghi@gmail.com>
2018-06-04 00:48:00 +0700

    login và signup với session

--- ProjectWebVersion2/controllers/signupController.js
+++ ProjectWebVersion2/controllers/signupController.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var signupRepo = require('../repos/signupRepo');
-
+var moment = require('moment');
 var router = express.Router();
 
 router.get('/', (req, res) => {
@@ -8,7 +8,20 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    signupRepo.add(req.body).then(value=>{
+
+    var dob = moment(req.body.dob, 'D/M/YYYY')
+        .format('YYYY-MM-DDTHH:mm');
+
+    var user = {
+        fullname : req.body.fullname,
+        email: req.body.email,
+        password: req.body.password,
+        cmnd: req.body.cmnd,
+        dob: req.body.dob,
+        permission: 0
+    };
+
+    signupRepo.add(user).then(value=>{
         res.redirect("/login");
     }).catch(err =>{
         console.log(err);
