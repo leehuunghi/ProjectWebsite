48c1ef0b7a1b970c9567919aac0ada99fc45fcb8
DESKTOP-AH2IB63\Windows 10 <leehuunghi@gmail.com>
2018-06-26 21:43:01 +0700

    xây dựng giao diện phân hệ admin ở express

--- ProjectWebVersion2/controllers/adminController.js
+++ ProjectWebVersion2/controllers/adminController.js
@@ -6,4 +6,20 @@ router.get('/', (req, res) => {
     res.render('admin/index', {layout: false});
 });
 
+router.get('/dashboard', (req, res) => {
+    res.render('admin/dashboard', {layout: 'admin.handlebars'});
+});
+
+router.get('/sanpham', (req, res) => {
+    res.render('admin/sanpham', {layout: 'admin.handlebars'});
+});
+
+router.get('/donhang', (req, res) => {
+    res.render('admin/donhang', {layout: 'admin.handlebars'});
+});
+
+router.get('/nguoidung', (req, res) => {
+    res.render('admin/nguoidung', {layout: 'admin.handlebars'});
+});
+
 module.exports = router;
