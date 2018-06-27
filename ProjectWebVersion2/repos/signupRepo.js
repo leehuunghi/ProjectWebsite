251417c5d8e178047c99f1837ee60a28e5aa8e59
DESKTOP-4H2CDN2\Admin <trannha132@gmail.com>
2018-06-27 23:45:35 +0700

    Update thông tin user: hoàn chỉnh, bắt lỗi nhập sai pass

--- ProjectWebVersion2/repos/signupRepo.js
+++ ProjectWebVersion2/repos/signupRepo.js
@@ -2,5 +2,5 @@ var db = require('../fn/db');
 
 exports.add = (user) => {
     var sql = `INSERT INTO users(email,hoten,sdt,matkhau,cmnd,NgaySinh,gioitinh) VALUES ('${user.email}', '${user.fullname}','${user.phone}' ,'${user.password}', '${user.cmnd}', '${user.dob}','${user.sex}')`;
-    return db.save(sql); 
+    return db.save(sql);
 }
