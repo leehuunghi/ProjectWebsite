6d549b79fae93fbd0341717298a3ba2b022ab7fe
DESKTOP-AH2IB63\Windows 10 <leehuunghi@gmail.com>
2018-06-28 13:04:53 +0700

    Đơn hàng chuẩn bị thanh toán

--- ProjectWebVersion2/controllers/packageController.js
+++ ProjectWebVersion2/controllers/packageController.js
@@ -1,9 +1,35 @@
 var express = require('express');
 
 var router = express.Router();
+var productRepo = require('../repos/productDetailRepo');
 
 router.get('/', (req, res) => {
-    res.render('package/index');
+    var arr_p = [];
+    for (var i = 0; i < req.session.cart.length; i++) {
+        var cartItem = req.session.cart[i];
+        var p = productRepo.loadFullProduct(cartItem.ProId0, cartItem.ProID);
+        arr_p.push(p);
+    }
+
+    var items = [];
+    var SumMoney=0;
+    Promise.all(arr_p).then(result => {
+        for (var i = result.length - 1; i >= 0; i--) {
+            var pro = result[i][0];
+            var item = {
+                Product: pro,
+                Quantity: req.session.cart[i].Quantity,
+                Amount: pro.Gia * req.session.cart[i].Quantity
+            };
+            SumMoney+=item.Amount;
+            items.push(item);
+        }
+        var vm = {
+            items: items,
+            SumMoney: SumMoney,
+        };
+        res.render('package/index',vm);
+    });
 });
 
 module.exports = router;
