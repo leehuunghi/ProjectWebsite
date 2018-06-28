aad70cc44095ad0779630d3c78b332b42c91d545
DESKTOP-4H2CDN2\Admin <trannha132@gmail.com>
2018-06-28 13:33:08 +0700

    hiển thị check đăng nhập

--- ProjectWebVersion2/controllers/loginController.js
+++ ProjectWebVersion2/controllers/loginController.js
@@ -17,7 +17,6 @@ router.post('/', (req, res) => {
     loginRepo.check(user).then(rows=>{
         if(rows!=null)
         {
-
             req.session.isLogged = true;
             req.session.user = rows;
             var mm=req.session.user.NgaySinh.getMonth()+1;
@@ -26,14 +25,19 @@ router.post('/', (req, res) => {
             if(mm<10) mm="0" + mm;
             if(dd<10) dd= "0" + dd;
             req.session.user.NgaySinh = yyyy +'-'+ mm  + '-' + dd;
-            console.log(req.session.user);
             var url = '/';
             if (req.query.retUrl) {
                 url = req.query.retUrl;
             }
             res.redirect(url);
         }
-        else res.render('login/index');
+        else
+        {
+            var vm={
+                notHas:true
+            }
+            res.render('login/index',vm);
+        }
     }
     ).catch(err=>{
         res.end("fail");
