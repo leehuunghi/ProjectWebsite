8f66171525b83b034fa452e2fc50c0514f3bf8dd
DESKTOP-4H2CDN2\Admin <trannha132@gmail.com>
2018-06-29 13:46:02 +0700

    admin: quản lý loại sp, nsx

--- ProjectWebVersion2/controllers/adminController.js
+++ ProjectWebVersion2/controllers/adminController.js
@@ -11,6 +11,26 @@ router.get('/dashboard', (req, res) => {
     res.render('admin/dashboard', {layout: 'admin.handlebars'});
 });
 
+router.get('/loaisanpham', (req, res) => {
+    adminProductRepo.loaiSP().then(rows=>{
+        vm={
+            LoaiSP: rows,
+            layout: 'admin.handlebars',
+        }
+        res.render('admin/loaisanpham', vm);
+    });
+});
+
+router.get('/nsx', (req, res) => {
+    adminProductRepo.nsx().then(rows=>{
+        vm={
+            NSX: rows,
+            layout: 'admin.handlebars',
+        }
+        res.render('admin/nsx', vm);
+    });
+});
+
 router.get('/sanpham', (req, res) => {
     var sp = adminProductRepo.loadSP();
     Promise.all([sp]).then(([rows1]) => {      
@@ -60,6 +80,16 @@ router.post('/sanpham/add', (req,res)=>{
         }
         res.render('admin/sanpham',vm);
     })
+})
+
+router.post('/loaisanpham', (req,res)=>{
+    var inputLoaiSP=req.body.TenLoai;
+    console.log(inputLoaiSP);
+    res.render('admin/loaisanpham',vm);
+})
+
+router.post('/nsx', (req,res)=>{
+    res.render('admin/nsx',vm);
 })
 
 module.exports = router;
--- ProjectWebVersion2/repos/adminProductRepo.js
+++ ProjectWebVersion2/repos/adminProductRepo.js
@@ -9,3 +9,14 @@ exports.loadSP = () => {
     var sql = `select sanpham.*,  (SELECT COALESCE(sum(soluong),0) FROM sanphamct WHERE sanphamct.ID0 = sanpham.ID) as TongSL from sanpham`;
     return db.load(sql);
 }
+
+exports.loaiSP = () => {
+    var sql = `select distinct(LoaiSP) as TenLoai, count(*) as SoLuong  from sanpham group by LoaiSP`;
+    return db.load(sql);
+}
+
+exports.nsx = () => {
+    var sql = `select distinct(NhaSanXuat) as TenNSX, count(*) as SoLuongSanPham  from sanpham group by NhaSanXuat`;
+    return db.load(sql);
+}
+
